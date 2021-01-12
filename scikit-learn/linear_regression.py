f248d30300fb3b01221a61ef57dc974479276c2f
bpesquet <bpesquet@gmail.com>
2021-01-12 17:16:29 +0100

    Update scikit-learn LR recipe

--- scikit-learn/linear_regression.py
+++ scikit-learn/linear_regression.py
@@ -5,31 +5,15 @@ Linear regression with scikit-learn.
 import matplotlib.pyplot as plt
 import numpy as np
 import sklearn
-from sklearn import linear_model
+from sklearn import linear_model, metrics
 
 print(f"scikit-learn version: {sklearn.__version__}")
-
-
-def train(model, x_train, y_train, num_epochs, learning_rate):
-    "Train e scikit-learn model"
-
-    model.fit(x_train, y_train)
-
-    # Plot data and predictions
-    y_pred = model.predict(x_train)
-    plt.plot(x_train, y_train, "ro", label="Data")
-    plt.plot(x_train, y_pred, label="Prediction")
-    plt.title(f"Linear Regression with scikit-learn (TODO)")
-    plt.legend()
-    plt.show()
 
 
 def main():
     """Main function"""
 
     # Hyper-parameters
-    input_size = 1
-    output_size = 1
     num_epochs = 60
     learning_rate = 0.001
 
@@ -56,35 +40,49 @@ def main():
 
     y_train = np.array(
         [
-            [1.7],
-            [2.76],
-            [2.09],
-            [3.19],
-            [1.694],
-            [1.573],
-            [3.366],
-            [2.596],
-            [2.53],
-            [1.221],
-            [2.827],
-            [3.465],
-            [1.65],
-            [2.904],
-            [1.3],
+            1.7,
+            2.76,
+            2.09,
+            3.19,
+            1.694,
+            1.573,
+            3.366,
+            2.596,
+            2.53,
+            1.221,
+            2.827,
+            3.465,
+            1.65,
+            2.904,
+            1.3,
         ]
     )
-
-    # Linear regression models
 
     # Using the Normal Equation
     model_normal = linear_model.LinearRegression()
 
     # Using SGD
-    model_sgd = linear_model.SGDRegressor()
+    model_sgd = linear_model.SGDRegressor(
+        max_iter=num_epochs, learning_rate="constant", eta0=learning_rate
+    )
+
+    # Linear regression models
+    model_list = [model_normal, model_sgd]
 
     # Train both models
-    for model in [model_normal, model_sgd]:
-        train(model, x_train, y_train, num_epochs, learning_rate)
+    for model in model_list:
+        model.fit(x_train, y_train)
+        final_loss = metrics.mean_squared_error(y_train, model.predict(x_train))
+        print(f"Final loss for {type(model).__name__}: {final_loss:.5f}")
+
+    # Plot data and predictions
+    plt.plot(x_train, y_train, "ro", label="Data")
+    for model in model_list:
+        y_pred = model.predict(x_train)
+        plt.plot(x_train, y_pred, label=type(model).__name__)
+    plt.title("Linear Regression with scikit-learn")
+    plt.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
